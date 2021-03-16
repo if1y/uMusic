1d75f325d9a128e9b2b6a574505f454cbcc0fcef
lincx <1367372276@qq.com>
2021-03-16 21:56:05 +0800

    首页

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ var app = express();
 const match = require('@nondanee/unblockneteasemusic');
 
 app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
-  if (req.method == 'OPTIONS') {
-    res.send(200);
-  }
-  else {
-    next();
-  }
+	if (req.method == 'OPTIONS') {
+		res.send(200);
+	}
+	else {
+		next();
+	}
+});
+
+app.get('/', function(res, rep) {
+	rep.send('index.html');
 });
 
 app.get('/getSong', function(res, rep) {
